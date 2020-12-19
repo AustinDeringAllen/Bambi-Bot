903f6caf999e488524255d01ab78fddbea008fbb
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-12-18 22:56:43 -0800

    added emote reactions

--- src/main/java/Bambi.java
+++ src/main/java/Bambi.java
@@ -55,6 +55,15 @@ public class Bambi {
                     }
                 });
 
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .subscribe(event -> {
+                    Message message = event.getMessage();
+                    if(message.getContent().equalsIgnoreCase("!me")) {
+                        MessageChannel channel = message.getChannel().block();
+                        channel.createMessage(message.getAuthor().map(User::getUsername).get()).block();
+
+                });
+
 
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .subscribe(event -> {
