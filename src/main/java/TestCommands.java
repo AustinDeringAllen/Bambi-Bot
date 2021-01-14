69c029cc1edad32658d9d5193a4fd19e0c5d84f0
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2021-01-13 18:13:31 -0800

    Moved the command to get a users avatar url

--- src/main/java/TestCommands.java
+++ src/main/java/TestCommands.java
@@ -2,6 +2,8 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import discord4j.core.object.entity.channel.MessageChannel;
 
 public class TestCommands {
     public static void ListenForCommands(GatewayDiscordClient client, String operator) {
@@ -12,5 +14,14 @@ public class TestCommands {
                 .flatMap(Message::getChannel)
                 .flatMap(channel -> channel.createMessage("Pong!"))
                 .subscribe();
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .subscribe(event -> {
+                    Message message = event.getMessage();
+                    if(message.getContent().equalsIgnoreCase("$me")) {
+                        MessageChannel channel = message.getChannel().block();
+                        channel.createMessage(message.getAuthor().map(User::getAvatarUrl).get()).block();
+                    }
+                });
     }
 }
