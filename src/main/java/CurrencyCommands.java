ce4552f3ec9e81ed922f768562ad05485a89121f
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2021-01-19 23:19:53 -0800

    Fixed command to check if user has money or not

--- src/main/java/CurrencyCommands.java
+++ src/main/java/CurrencyCommands.java
@@ -18,7 +18,7 @@ public class CurrencyCommands {
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .subscribe(event -> {
                     Message message = event.getMessage();
-                    if (message.getContent().equalsIgnoreCase("!register")) {
+                    if (message.getContent().equalsIgnoreCase(operator + "register")) {
                         String username = message.getAuthor().map(User::getUsername).get();
                         Snowflake userId = message.getAuthor().map(User::getId).get();
                         if (!currency.containsKey(username)) {
@@ -29,87 +29,20 @@ public class CurrencyCommands {
                         }
                     }
                 });
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .subscribe(event -> {
+                    Message message = event.getMessage();
+                    MessageChannel channel = message.getChannel().block();
+                    if (message.getContent().equalsIgnoreCase(operator + "currency")) {
+                        String username = message.getAuthor().map(User::getUsername).get();
+                        Long userId = message.getAuthor().map(User::getId).get().asLong();
+                        if (currency.containsKey(userId)) {
+                            channel.createMessage(username + " has " + currency.get(userId) + " money").block();
+                        } else {
+                            channel.createMessage(username + " doesn't exist within our database").block();
+                        }
+                    }
+                });
     }
-
-
 }
-
-//        client.getEventDispatcher().on(MessageCreateEvent.class)
-//                .subscribe(event -> {
-//                    Message message = event.getMessage();
-//                    MessageChannel channel = message.getChannel().block();
-//                    if(message.getContent().equalsIgnoreCase("!currency")) {
-//                        String username = message.getAuthor().map(User::getUsername).get();
-//                        if(currency.containsKey(username)) {
-//                            channel.createMessage(username + " has " + currency.get(username) + " money");
-//                        } else {
-//                            channel.createMessage(username + " doesn't exist within our database");
-//                        }
-//                    }
-//                });
-//
-//        client.getEventDispatcher().on(MessageCreateEvent.class)
-//                .subscribe(event -> {
-//                    Message message = event.getMessage();
-//                    MessageChannel channel = message.getChannel().block();
-//                    if(message.getContent().contains("!pay")) {
-//                        String[] userInput = message.toString().split(" ");
-//                        String username = message.getAuthor().map(User::getUsername).get();
-//                        String recipient = "";
-//                        long amount = Long.parseLong(userInput[userInput.length-1]);
-//                        for(int i=1; i<userInput.length-1; i++) {
-//                            recipient += userInput[i];
-//                        }
-//
-//                        if(currency.containsKey(username)) {
-//                            if(currency.containsKey(recipient)) {
-//                                if(currency.get(username) - amount < 0) {
-//                                    currency.put(username, currency.get(username) - amount);
-//                                    currency.put(recipient, currency.get(recipient + amount));
-//                                    channel.createMessage("Transaction Complete");
-//                                } else {
-//                                    channel.createMessage("User trying to send money does not have enough");
-//                                }
-//                            } else {
-//                                channel.createMessage("User to receive send money isn't registered");
-//                            }
-//                        } else {
-//                            channel.createMessage("User trying to send money isn't registered");
-//                        }
-//                    }
-//                });
-//    }
-//
-//    public static int checkForErrors(String username, String recipient, long amount) {
-//        if(!currency.containsKey(username)) {
-//            return 1;
-//        }
-//
-//        if(!currency.containsKey(recipient))
-//            return 2;
-//
-//        if(currency.get(username) - amount < 0)
-//            return 3;
-//
-//        return 0;
-//    };
-//
-//    public static boolean returnErrorMessage(int errorNumber, MessageChannel channel) {
-//        switch (errorNumber) {
-//            case 1:
-//                channel.createMessage("User trying to send money isn't registered");
-//                return false;
-//
-//            case 2:
-//                channel.createMessage("User to receive send money isn't registered");
-//                return false;
-//
-//            case 3:
-//                channel.createMessage("User trying to send money doesn't have enough");
-//                return false;
-//
-//            default:
-//                return true;
-//        }
-//    }
-//}
