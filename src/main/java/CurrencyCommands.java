5203ad15c0828cbc90e33d80e97d566384b9db9a
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2021-01-20 23:21:51 -0800

    Splits pay command and puts things where they need to be respectively

--- src/main/java/CurrencyCommands.java
+++ src/main/java/CurrencyCommands.java
@@ -50,8 +50,77 @@ public class CurrencyCommands {
                    Message message = event.getMessage();
                    MessageChannel channel = message.getChannel().block();
                    if(message.getContent().contains(operator + "pay")) {
-
+                       String[] userInput = message.getContent().split(" ");
+                       Long userId = message.getAuthor().map(User::getId).get().asLong();
+                       Long recipientId = Long.parseLong(userInput[1].substring(3,userInput[1].length()-1));
+                       Long amount = Long.parseLong(userInput[2]);
                    }
                 });
     }
 }
+//
+//        client.getEventDispatcher().on(MessageCreateEvent.class)
+//                .subscribe(event -> {
+//                    Message message = event.getMessage();
+//                    MessageChannel channel = message.getChannel().block();
+//                    if(message.getContent().contains("!pay")) {
+//                        String[] userInput = message.toString().split(" ");
+//                        String username = message.getAuthor().map(User::getUsername).get();
+//                        String recipient = "";
+//                        long amount = Long.parseLong(userInput[userInput.length-1]);
+//                        for(int i=1; i<userInput.length-1; i++) {
+//                            recipient += userInput[i];
+//                        }
+//
+//                        if(currency.containsKey(username)) {
+//                            if(currency.containsKey(recipient)) {
+//                                if(currency.get(username) - amount < 0) {
+//                                    currency.put(username, currency.get(username) - amount);
+//                                    currency.put(recipient, currency.get(recipient + amount));
+//                                    channel.createMessage("Transaction Complete");
+//                                } else {
+//                                    channel.createMessage("User trying to send money does not have enough");
+//                                }
+//                            } else {
+//                                channel.createMessage("User to receive send money isn't registered");
+//                            }
+//                        } else {
+//                            channel.createMessage("User trying to send money isn't registered");
+//                        }
+//                    }
+//                });
+//    }
+//
+//    public static int checkForErrors(String username, String recipient, long amount) {
+//        if(!currency.containsKey(username)) {
+//            return 1;
+//        }
+//
+//        if(!currency.containsKey(recipient))
+//            return 2;
+//
+//        if(currency.get(username) - amount < 0)
+//            return 3;
+//
+//        return 0;
+//    };
+//
+//    public static boolean returnErrorMessage(int errorNumber, MessageChannel channel) {
+//        switch (errorNumber) {
+//            case 1:
+//                channel.createMessage("User trying to send money isn't registered");
+//                return false;
+//
+//            case 2:
+//                channel.createMessage("User to receive send money isn't registered");
+//                return false;
+//
+//            case 3:
+//                channel.createMessage("User trying to send money doesn't have enough");
+//                return false;
+//
+//            default:
+//                return true;
+//        }
+//    }
+//}
